e094979610aa78e5f9a473bb82366f7af5b45709
IvanIglesias7 <csi21-iiglhez@colegioaltair.net>
2023-09-27 13:18:22 +0200

    first commit

---
+++ conexionPostgreSQLC#/Conexion/conexion.cs
@@ -0,0 +1,47 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conexionPostgreSQLC_.Conexion
+{
+    public class conexion
+    {
+        NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432; User Id=postgres;Password=_altair458;Database =gestorBibliotecaPersonal");
+
+        public void conectarBD()
+        {
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Conexion satisfactoria");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            conn.Close();
+        }
+        
+        public void selectAllBD()
+        {
+            NpgsqlCommand npc = new NpgsqlCommand("SELECT * FROM gbp_almacen.gbp_alm_cat_libros");
+            npc.Connection = conn;
+            conn.Open();
+            NpgsqlDataReader dr = npc.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.Write("{0}\t{1}\t{2}\t{3}\t{4}", dr[0], dr[1], dr[2], dr[3], dr[4]);
+                Console.WriteLine(" ");
+            }
+            conn.Close(); 
+        }
+
+
+
+    }
+}
---
+++ conexionPostgreSQLC#/Program.cs
@@ -0,0 +1,23 @@
+﻿using conexionPostgreSQLC_.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conexionPostgreSQLC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            conexion c = new conexion();
+
+            c.conectarBD();
+
+            c.selectAllBD();
+
+            Console.ReadLine();
+        }
+    }
+}
